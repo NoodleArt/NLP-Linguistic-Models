dfe6960092aec008c86bab2bb307eb05897085dd
NoodleArt <135625396+NoodleArt@users.noreply.github.com>
2022-06-17 17:11:36 +0000

    - WL#6470: merge from mysql-trunk

---
+++ LanguageModelling/lan_model.py
@@ -0,0 +1,6 @@
+# Goal:
+# Apply different language models like unigram, bigram, trigram on the given twitter corpus and codemixed corpus.
+# Find CMI and Perplexity for each of the above models.
+# Compare perplexity and analyse the best among them.
+
+#
